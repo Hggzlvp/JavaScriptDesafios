c3cbec67150d7870b42f738dbaf81ce78e3957e1
Hernan Garcia <hernangarcia706@gmail.com>
2021-12-18 18:55:47 -0300

    Desafios 1 y 2

---
+++ javascript.js
@@ -0,0 +1,19 @@
+// DESAFIO 1 //
+let nombre=prompt("Ingresar tu nombre")
+
+let apellido=prompt("Ingresar tu apellido")
+
+alert ("Hola,"+" "+nombre +" "+ apellido)
+
+// DESAFIO 2 //
+
+let numero1=prompt("Insertar un numero entre 100 y 200")
+
+if (numero1 >= 100 && numero1 <= 200)
+    {
+          alert("El numero ingresado esta entre 100 y 200")
+    }
+ else
+    {
+          alert("El numero ingresado no esta entre 100 y 200")
+    }
